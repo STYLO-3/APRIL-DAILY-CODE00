c113b30ce94805c77ce6427eb6bde4c1b6ed0514
STYLO-3 <35541285+STYLO-3@users.noreply.github.com>
2018-03-19 12:42:11 -0600

    Add files via upload

---
+++ march.19.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int main() {
+	while (1)
+	MessageBox(NULL, ("it doesnt surprise me that you suck"), ("your wack"), MB_OK | MB_SYSTEMMODAL);
+
+
+
+
+
+
+
+}
